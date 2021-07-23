4716735ba442e348ca35a8bd374a3d729040efb0
Joachim Ekström <Joachim.Ekstrom@yaramarine.com>
2021-07-23 15:22:31 +0200

    Fixed stuff to get it more efficent, however when fetch from Chrome twice the objects get here...

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 4000
+var cors = require('cors')
+app.use(cors())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 const sqlite3 = require('sqlite3').verbose();
@@ -30,35 +32,56 @@ app.get('/all', (req, res) => {
 
 })
 
-app.post('/addrow', function (req, res) {
+app.post('/addrows', function (req, res) {
     let db = new sqlite3.Database('./db/myDb.db');
+    console.log(req.body[1])
+    let rows = req.body.map(()=> "(?, ?, ?, ?, ?, ?, ?, ?)").join(', ')
+    //console.log(rows)
+    const sqlQuery = `INSERT INTO test(sname, bisactive, bisacknowledged, salarmtext, tactivestarttime, tacktime, tclearedtime, uirepeatcount) VALUES ` + rows
+    //console.log(req.body[0])
 
-    // console.log(req.body.sName)
-    // console.log(req.body.bIsActive)
-    // console.log(req.body.bIsAcknowledged)
-    // console.log(req.body.sAlarmText)
-    // console.log(req.body.tActiveStartTime)
-    // console.log(req.body.tAcknowlegedTime)
-    // console.log(req.body.tClearedTime)
-    // console.log(req.body.uiRepeatCount)
+    let dbElement = []
+    // fix incomming data.
+    for (let i=0; i < req.body.length; i++){
+        dbElement.push(req.body[i].sName)
+        dbElement.push(req.body[i].bIsActive)
+        dbElement.push(req.body[i].bIsAcknowledged)
+        dbElement.push(req.body[i].sAlarmText)
+        dbElement.push(req.body[i].tActiveStartTime)
+        dbElement.push(req.body[i].tAcknowlegedTime)
+        dbElement.push(req.body[i].tClearedTime)
+        dbElement.push(req.body[i].uiRepeatCount)
+    }
 
-    db.run(`INSERT INTO test(sname, bisactive, bisacknowledged, salarmtext, tactivestarttime, tacktime, tclearedtime, uirepeatcount) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`, 
-    [req.body.sName, req.body.bIsActive, req.body.bIsAcknowledged, req.body.sAlarmText, req.body.tActiveStartTime, 
-        req.body.tAcknowlegedTime, req.body.tClearedTime, req.body.uiRepeatCount], 
-        function(err) {
+    //db.serialize(function(){
+        db.run(sqlQuery, dbElement, function(err){
             if (err) {
                 console.log(err.message);
                 res.status(400).end()
             } else {
-                // get the last insert id
-                //console.log(this.lastID);
-                //console.log(`Added post with id ${this.lastID}\n`)
                 res.status(204).end()
             }
-
         });
+    //});
+
+    // db.run(`INSERT INTO test(sname, bisactive, bisacknowledged, salarmtext, tactivestarttime, tacktime, tclearedtime, uirepeatcount) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`, 
+    // [req.body.sName, req.body.bIsActive, req.body.bIsAcknowledged, req.body.sAlarmText, req.body.tActiveStartTime, 
+    //     req.body.tAcknowlegedTime, req.body.tClearedTime, req.body.uiRepeatCount], 
+    //     function(err) {
+    //         if (err) {
+    //             console.log(err.message);
+    //             res.status(400).end()
+    //         } else {
+    //             // get the last insert id
+    //             //console.log(this.lastID);
+    //             //console.log(`Added post with id ${this.lastID}\n`)
+    //             res.status(204).end()
+    //         }
+
+    //     });
+    //statement.finalize();
           // close the database connection
-        db.close();
+    db.close();
     
   })
 
